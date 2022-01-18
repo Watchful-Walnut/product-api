e24a76264cbcf0f0ce4b9624ee45e69cf2c889dd
Brandon Tsai <bjt888@gmail.com>
2022-01-18 15:49:00 -0800

    implements k6 test

--- server/index.js
+++ server/index.js
@@ -6,10 +6,18 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/api/products', controller.getProducts);
-app.get('/api/products/:product_id', controller.getSingleProduct);
-app.get('/api/products/:product_id/styles', controller.getStyles);
-app.get('/api/products/:product_id/related', controller.getRelated);
+// ROUTES
+
+// GET REQUESTS
+app.get('/products', controller.getProducts);
+app.get('/products/:product_id', controller.getSingleProduct);
+app.get('/products/:product_id/styles', controller.getStyles);
+app.get('/products/:product_id/related', controller.getRelated);
+
+// TESTING
+app.get('/test', controller.getSingleProductTest);
+app.get('/test/styles', controller.getStylesTest);
+app.get('/test/related', controller.getRelatedTest);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
